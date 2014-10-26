40ad020927a67c563be4ca9b1d6cc2f3dbdc28a1
kovalikp <kovalikp@gmail.com>
2014-10-26 16:55:26 +0100

    Fixed assign target framework and test

---
+++ src/NuProj.Tests/Infrastructure/PackageDependencyComparer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using NuGet;
+
+namespace NuProj.Tests.Infrastructure
+{
+    public class PackageDependencyComparer : IEqualityComparer<PackageDependency>
+    {
+        private StringComparer _stringComparer;
+
+        public PackageDependencyComparer(StringComparer stringComparer)
+        {
+            if (stringComparer == null)
+            {
+                throw new ArgumentNullException("stringComparer");
+            }
+            _stringComparer = stringComparer;
+        }
+
+        public bool Equals(PackageDependency x, PackageDependency y)
+        {
+            if (x == null && x == null)
+            {
+                return true;
+            }
+
+            if (x == null || y == null)
+            {
+                return false;
+            }
+
+            var xVersionSpec = x.VersionSpec == null ? "" : x.VersionSpec.ToString();
+            var yVersionSpec = y.VersionSpec == null ? "" : y.VersionSpec.ToString();
+            return _stringComparer.Equals(x.Id, y.Id)
+                && _stringComparer.Equals(xVersionSpec, yVersionSpec);
+        }
+
+        public int GetHashCode(PackageDependency obj)
+        {
+            if (obj == null)
+            {
+                throw new ArgumentNullException("obj");
+            }
+
+            var versionSpec = obj.VersionSpec == null ? "" : obj.VersionSpec.ToString();
+            return _stringComparer.GetHashCode(obj.Id ?? "") + _stringComparer.GetHashCode(versionSpec);
+        }
+    }
+}
---
+++ src/NuProj.Tests/Infrastructure/PackageDependencySetComparer.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NuGet;
+
+namespace NuProj.Tests.Infrastructure
+{
+    public class PackageDependencySetComparer : IEqualityComparer<PackageDependencySet>
+    {
+        private static PackageDependencySetComparer _instance = new PackageDependencySetComparer(StringComparer.OrdinalIgnoreCase);
+        private PackageDependencyComparer _packageDependencyComparer;
+        private StringComparer _stringComparer;
+
+        public PackageDependencySetComparer(StringComparer stringComparer)
+        {
+            if (stringComparer == null)
+            {
+                throw new ArgumentNullException("stringComparer");
+            }
+            _stringComparer = stringComparer;
+            _packageDependencyComparer = new PackageDependencyComparer(stringComparer);
+        }
+
+        public static PackageDependencySetComparer Instance
+        {
+            get
+            {
+                return _instance;
+            }
+        }
+
+        public bool Equals(PackageDependencySet x, PackageDependencySet y)
+        {
+            if (x == null && x == null)
+            {
+                return true;
+            }
+
+            if (x == null || y == null)
+            {
+                return false;
+            }
+
+            var xDependencies = new HashSet<PackageDependency>(
+                x.Dependencies ?? Enumerable.Empty<PackageDependency>(),
+                _packageDependencyComparer);
+
+            var yDependencies = new HashSet<PackageDependency>(
+                y.Dependencies ?? Enumerable.Empty<PackageDependency>(),
+                _packageDependencyComparer);
+
+            return x.TargetFramework == y.TargetFramework
+                && xDependencies.SetEquals(yDependencies);
+        }
+
+        public int GetHashCode(PackageDependencySet obj)
+        {
+            if (obj == null)
+            {
+                throw new ArgumentNullException("obj");
+            }
+
+            return obj.TargetFramework == null ? 0 : obj.TargetFramework.GetHashCode();
+        }
+    }
+}
